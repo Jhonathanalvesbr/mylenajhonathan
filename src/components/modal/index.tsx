4de86945b006a1eb334f08483d8f3cc0cbffbef4
Jhonathan Alves <Jhonathanalves_br@hotmail.com>
2024-08-19 21:23:09 -0300

    fix: texto

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -127,7 +127,7 @@ const ModalImport: React.FC = () => {
                 >
                     <Result
                         status="info"
-                        title="Confime sua presença"
+                        title="Confime a sua presença"
                         extra={
                             <Button
                                 type="primary"
